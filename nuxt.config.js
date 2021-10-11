2657f0aeebf1ce25dbe65a374446c8d15e84c660
Eries Trisnadi <lognus21@gmail.com>
2021-10-11 18:48:11 +0700

    fix: add meta tags
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,16 @@
+import generateMetaTags from './utils/generate-meta-tags'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: process.env.APP_TITLE || 'Document',
+    title: process.env.SITE_TITLE || 'Document',
     meta: [
+      ...generateMetaTags(),
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
---
+++ utils/generate-meta-tags.ts
@@ -0,0 +1,82 @@
+export type MetaOption = {
+  title?: string
+  description?: string
+  type?: string
+  url?: string
+  image?: string
+}
+
+export const generateMetaTags = (meta?: MetaOption) => {
+  return [
+    {
+      hid: 'apple-mobile-web-app-title',
+      name: 'apple-mobile-web-app-title',
+      content: (meta && meta.title) || process.env.SITE_TITLE,
+    },
+    {
+      hid: 'description',
+      name: 'description',
+      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+    },
+    {
+      hid: 'og:site_name',
+      name: 'og:site_name',
+      content: (meta && meta.title) || process.env.SITE_TITLE,
+    },
+    {
+      hid: 'og:type',
+      property: 'og:type',
+      content: (meta && meta.type) || 'website',
+    },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: (meta && meta.url) || process.env.SITE_URL,
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content:
+        (meta && meta.title) ||
+        'Kirim Paket, Barang & Kargo dengan Berbagai Kurir Seluruh Indonesia | ' +
+          process.env.SITE_TITLE,
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: (meta && meta.image) || process.env.SITE_IMAGE,
+    },
+    {
+      hid: 'twitter:url',
+      name: 'twitter:url',
+      content: (meta && meta.url) || process.env.SITE_URL,
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content:
+        (meta && meta.title) ||
+        'Kirim Paket, Barang & Kargo dengan Berbagai Kurir Seluruh Indonesia | ' +
+          process.env.SITE_TITLE,
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: (meta && meta.image) || process.env.SITE_IMAGE,
+    },
+  ]
+    .filter(({ content }) => !!content)
+    .map((v) => ({ ...v, content: v.content || '' }))
+}
+
+export default generateMetaTags
