801b510663cc72b20a420ebca8bd0789bc40518d
Eries Trisnadi <lognus21@gmail.com>
2021-10-17 03:18:39 +0700

    fix: use h4 instead for home hero description
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

--- components/v1.0/features/home/hero/index.vue
+++ components/v1.0/features/home/hero/index.vue
@@ -2,9 +2,9 @@
   <comm-hero>
     <div class="max-w-screen-sm lg:max-w-lg">
       <h1 class="mb-5 text-5xl font-bold text-center">{{ title }}</h1>
-      <p v-if="!!description" class="mb-5 text-center">
+      <h4 v-if="!!description" class="mb-5 text-center">
         {{ description }}
-      </p>
+      </h4>
       <slot />
     </div>
   </comm-hero>
