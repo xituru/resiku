a512864a365e187529b09b9976f619e080607c3a
Eries Trisnadi <lognus21@gmail.com>
2021-11-19 02:07:49 +0700

    fix: update meta tag utils
    Signed-off-by: Eries Trisnadi <lognus21@gmail.com>

--- utils/generate-meta-tags.ts
+++ utils/generate-meta-tags.ts
@@ -1,3 +1,5 @@
+import get from './get'
+
 export type MetaOption = {
   title?: string
   description?: string
@@ -7,66 +9,75 @@ export type MetaOption = {
 }
 
 export const generateMetaTags = (meta?: MetaOption) => {
+  const getMeta = (key: string, fallback?: string) =>
+    get(
+      meta,
+      'meta.' + String(key).toLowerCase(),
+      get(process.env, 'SITE_' + String(key).toUpperCase(), fallback || key)
+    )
+  const mergeMeta = (a: string, b: string, separator: string = ' | ') =>
+    [getMeta(a), getMeta(b)].join(separator)
+
   return [
     {
       hid: 'apple-mobile-web-app-title',
       name: 'apple-mobile-web-app-title',
-      content: (meta && meta.title) || process.env.SITE_TITLE,
+      content: getMeta('title'),
     },
     {
       hid: 'description',
       name: 'description',
-      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+      content: getMeta('description'),
     },
     {
       hid: 'og:site_name',
       name: 'og:site_name',
-      content: (meta && meta.title) || process.env.SITE_TITLE,
+      content: getMeta('title'),
     },
     {
       hid: 'og:type',
       property: 'og:type',
-      content: (meta && meta.type) || 'website',
+      content: getMeta('type', 'website'),
     },
     {
       hid: 'og:url',
       property: 'og:url',
-      content: (meta && meta.url) || process.env.SITE_URL,
+      content: getMeta('url'),
     },
     {
       hid: 'og:title',
       property: 'og:title',
-      content: (meta && meta.title) || process.env.SITE_TITLE,
+      content: mergeMeta('description', 'title'),
     },
     {
       hid: 'og:description',
       property: 'og:description',
-      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+      content: getMeta('description'),
     },
     {
       hid: 'og:image',
       property: 'og:image',
-      content: (meta && meta.image) || process.env.SITE_IMAGE,
+      content: getMeta('image'),
     },
     {
       hid: 'twitter:url',
       name: 'twitter:url',
-      content: (meta && meta.url) || process.env.SITE_URL,
+      content: getMeta('url'),
     },
     {
       hid: 'twitter:title',
       name: 'twitter:title',
-      content: (meta && meta.title) || process.env.SITE_TITLE,
+      content: mergeMeta('description', 'title'),
     },
     {
       hid: 'twitter:description',
       name: 'twitter:description',
-      content: (meta && meta.description) || process.env.SITE_DESCRIPTION,
+      content: getMeta('description'),
     },
     {
       hid: 'twitter:image',
       name: 'twitter:image',
-      content: (meta && meta.image) || process.env.SITE_IMAGE,
+      content: getMeta('image'),
     },
   ]
     .filter(({ content }) => !!content)
